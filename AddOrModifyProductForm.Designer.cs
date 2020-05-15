6d653641b9b984892814e30bc19c8a312cb73631
Louie Gara <louie.gara@outlook.com>
2020-05-15 11:45:52 -0400

    added the add or modify product ui

--- AddOrModifyProductForm.Designer.cs
+++ AddOrModifyProductForm.Designer.cs
@@ -28,12 +28,288 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.allPartsListView = new System.Windows.Forms.ListView();
+			this.associatedPartsListView = new System.Windows.Forms.ListView();
+			this.allCandidatePartsLabel = new System.Windows.Forms.Label();
+			this.associatedPartsLabel = new System.Windows.Forms.Label();
+			this.candidatePartSearchButton = new System.Windows.Forms.Button();
+			this.candidatePartSearchTextBox = new System.Windows.Forms.TextBox();
+			this.candidatePartsAddButton = new System.Windows.Forms.Button();
+			this.associatedPartDeleteButton = new System.Windows.Forms.Button();
+			this.addOrModifyProductSaveButton = new System.Windows.Forms.Button();
+			this.addOrModifyProductCancelButton = new System.Windows.Forms.Button();
+			this.addOrModifyProductLabel = new System.Windows.Forms.Label();
+			this.idLabel = new System.Windows.Forms.Label();
+			this.nameLabel = new System.Windows.Forms.Label();
+			this.inventoryLabel = new System.Windows.Forms.Label();
+			this.priceLabel = new System.Windows.Forms.Label();
+			this.minLabel = new System.Windows.Forms.Label();
+			this.maxLabel = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// allPartsListView
+			// 
+			this.allPartsListView.HideSelection = false;
+			this.allPartsListView.Location = new System.Drawing.Point(418, 103);
+			this.allPartsListView.Name = "allPartsListView";
+			this.allPartsListView.Size = new System.Drawing.Size(388, 143);
+			this.allPartsListView.TabIndex = 0;
+			this.allPartsListView.UseCompatibleStateImageBehavior = false;
+			// 
+			// associatedPartsListView
+			// 
+			this.associatedPartsListView.HideSelection = false;
+			this.associatedPartsListView.Location = new System.Drawing.Point(418, 299);
+			this.associatedPartsListView.Name = "associatedPartsListView";
+			this.associatedPartsListView.Size = new System.Drawing.Size(388, 143);
+			this.associatedPartsListView.TabIndex = 1;
+			this.associatedPartsListView.UseCompatibleStateImageBehavior = false;
+			// 
+			// allCandidatePartsLabel
+			// 
+			this.allCandidatePartsLabel.AutoSize = true;
+			this.allCandidatePartsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.allCandidatePartsLabel.Location = new System.Drawing.Point(415, 80);
+			this.allCandidatePartsLabel.Name = "allCandidatePartsLabel";
+			this.allCandidatePartsLabel.Size = new System.Drawing.Size(144, 17);
+			this.allCandidatePartsLabel.TabIndex = 2;
+			this.allCandidatePartsLabel.Text = "All candidate parts";
+			// 
+			// associatedPartsLabel
+			// 
+			this.associatedPartsLabel.AutoSize = true;
+			this.associatedPartsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.associatedPartsLabel.Location = new System.Drawing.Point(415, 279);
+			this.associatedPartsLabel.Name = "associatedPartsLabel";
+			this.associatedPartsLabel.Size = new System.Drawing.Size(253, 17);
+			this.associatedPartsLabel.TabIndex = 3;
+			this.associatedPartsLabel.Text = "Parts associated with this product";
+			// 
+			// candidatePartSearchButton
+			// 
+			this.candidatePartSearchButton.Location = new System.Drawing.Point(572, 74);
+			this.candidatePartSearchButton.Name = "candidatePartSearchButton";
+			this.candidatePartSearchButton.Size = new System.Drawing.Size(75, 23);
+			this.candidatePartSearchButton.TabIndex = 4;
+			this.candidatePartSearchButton.Text = "Search";
+			this.candidatePartSearchButton.UseVisualStyleBackColor = true;
+			// 
+			// candidatePartSearchTextBox
+			// 
+			this.candidatePartSearchTextBox.Location = new System.Drawing.Point(653, 76);
+			this.candidatePartSearchTextBox.Name = "candidatePartSearchTextBox";
+			this.candidatePartSearchTextBox.Size = new System.Drawing.Size(153, 20);
+			this.candidatePartSearchTextBox.TabIndex = 5;
+			// 
+			// candidatePartsAddButton
+			// 
+			this.candidatePartsAddButton.Location = new System.Drawing.Point(731, 250);
+			this.candidatePartsAddButton.Name = "candidatePartsAddButton";
+			this.candidatePartsAddButton.Size = new System.Drawing.Size(75, 23);
+			this.candidatePartsAddButton.TabIndex = 6;
+			this.candidatePartsAddButton.Text = "Add";
+			this.candidatePartsAddButton.UseVisualStyleBackColor = true;
+			// 
+			// associatedPartDeleteButton
+			// 
+			this.associatedPartDeleteButton.Location = new System.Drawing.Point(731, 448);
+			this.associatedPartDeleteButton.Name = "associatedPartDeleteButton";
+			this.associatedPartDeleteButton.Size = new System.Drawing.Size(75, 23);
+			this.associatedPartDeleteButton.TabIndex = 7;
+			this.associatedPartDeleteButton.Text = "Delete";
+			this.associatedPartDeleteButton.UseVisualStyleBackColor = true;
+			// 
+			// addOrModifyProductSaveButton
+			// 
+			this.addOrModifyProductSaveButton.Location = new System.Drawing.Point(650, 495);
+			this.addOrModifyProductSaveButton.Name = "addOrModifyProductSaveButton";
+			this.addOrModifyProductSaveButton.Size = new System.Drawing.Size(75, 23);
+			this.addOrModifyProductSaveButton.TabIndex = 8;
+			this.addOrModifyProductSaveButton.Text = "Save";
+			this.addOrModifyProductSaveButton.UseVisualStyleBackColor = true;
+			// 
+			// addOrModifyProductCancelButton
+			// 
+			this.addOrModifyProductCancelButton.Location = new System.Drawing.Point(731, 495);
+			this.addOrModifyProductCancelButton.Name = "addOrModifyProductCancelButton";
+			this.addOrModifyProductCancelButton.Size = new System.Drawing.Size(75, 23);
+			this.addOrModifyProductCancelButton.TabIndex = 9;
+			this.addOrModifyProductCancelButton.Text = "Cancel";
+			this.addOrModifyProductCancelButton.UseVisualStyleBackColor = true;
+			// 
+			// addOrModifyProductLabel
+			// 
+			this.addOrModifyProductLabel.AutoSize = true;
+			this.addOrModifyProductLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.addOrModifyProductLabel.Location = new System.Drawing.Point(13, 13);
+			this.addOrModifyProductLabel.Name = "addOrModifyProductLabel";
+			this.addOrModifyProductLabel.Size = new System.Drawing.Size(100, 18);
+			this.addOrModifyProductLabel.TabIndex = 10;
+			this.addOrModifyProductLabel.Text = "Add Product";
+			// 
+			// idLabel
+			// 
+			this.idLabel.AutoSize = true;
+			this.idLabel.Location = new System.Drawing.Point(59, 200);
+			this.idLabel.Name = "idLabel";
+			this.idLabel.Size = new System.Drawing.Size(18, 13);
+			this.idLabel.TabIndex = 11;
+			this.idLabel.Text = "ID";
+			// 
+			// nameLabel
+			// 
+			this.nameLabel.AutoSize = true;
+			this.nameLabel.Location = new System.Drawing.Point(59, 226);
+			this.nameLabel.Name = "nameLabel";
+			this.nameLabel.Size = new System.Drawing.Size(35, 13);
+			this.nameLabel.TabIndex = 12;
+			this.nameLabel.Text = "Name";
+			// 
+			// inventoryLabel
+			// 
+			this.inventoryLabel.AutoSize = true;
+			this.inventoryLabel.Location = new System.Drawing.Point(59, 255);
+			this.inventoryLabel.Name = "inventoryLabel";
+			this.inventoryLabel.Size = new System.Drawing.Size(51, 13);
+			this.inventoryLabel.TabIndex = 13;
+			this.inventoryLabel.Text = "Inventory";
+			// 
+			// priceLabel
+			// 
+			this.priceLabel.AutoSize = true;
+			this.priceLabel.Location = new System.Drawing.Point(59, 283);
+			this.priceLabel.Name = "priceLabel";
+			this.priceLabel.Size = new System.Drawing.Size(31, 13);
+			this.priceLabel.TabIndex = 14;
+			this.priceLabel.Text = "Price";
+			// 
+			// minLabel
+			// 
+			this.minLabel.AutoSize = true;
+			this.minLabel.Location = new System.Drawing.Point(59, 309);
+			this.minLabel.Name = "minLabel";
+			this.minLabel.Size = new System.Drawing.Size(24, 13);
+			this.minLabel.TabIndex = 15;
+			this.minLabel.Text = "Min";
+			// 
+			// maxLabel
+			// 
+			this.maxLabel.AutoSize = true;
+			this.maxLabel.Location = new System.Drawing.Point(192, 309);
+			this.maxLabel.Name = "maxLabel";
+			this.maxLabel.Size = new System.Drawing.Size(27, 13);
+			this.maxLabel.TabIndex = 16;
+			this.maxLabel.Text = "Max";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(116, 200);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 17;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(116, 226);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 18;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(116, 255);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 19;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(116, 281);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 20;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(116, 309);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(70, 20);
+			this.textBox6.TabIndex = 21;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(225, 309);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(70, 20);
+			this.textBox7.TabIndex = 22;
+			// 
+			// AddOrModifyProductForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "AddOrModifyProductForm";
+			this.ClientSize = new System.Drawing.Size(868, 530);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.maxLabel);
+			this.Controls.Add(this.minLabel);
+			this.Controls.Add(this.priceLabel);
+			this.Controls.Add(this.inventoryLabel);
+			this.Controls.Add(this.nameLabel);
+			this.Controls.Add(this.idLabel);
+			this.Controls.Add(this.addOrModifyProductLabel);
+			this.Controls.Add(this.addOrModifyProductCancelButton);
+			this.Controls.Add(this.addOrModifyProductSaveButton);
+			this.Controls.Add(this.associatedPartDeleteButton);
+			this.Controls.Add(this.candidatePartsAddButton);
+			this.Controls.Add(this.candidatePartSearchTextBox);
+			this.Controls.Add(this.candidatePartSearchButton);
+			this.Controls.Add(this.associatedPartsLabel);
+			this.Controls.Add(this.allCandidatePartsLabel);
+			this.Controls.Add(this.associatedPartsListView);
+			this.Controls.Add(this.allPartsListView);
+			this.MaximumSize = new System.Drawing.Size(884, 569);
+			this.MinimumSize = new System.Drawing.Size(884, 569);
+			this.Name = "AddOrModifyProductForm";
+			this.Text = "Add Produuct";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ListView allPartsListView;
+		private System.Windows.Forms.ListView associatedPartsListView;
+		private System.Windows.Forms.Label allCandidatePartsLabel;
+		private System.Windows.Forms.Label associatedPartsLabel;
+		private System.Windows.Forms.Button candidatePartSearchButton;
+		private System.Windows.Forms.TextBox candidatePartSearchTextBox;
+		private System.Windows.Forms.Button candidatePartsAddButton;
+		private System.Windows.Forms.Button associatedPartDeleteButton;
+		private System.Windows.Forms.Button addOrModifyProductSaveButton;
+		private System.Windows.Forms.Button addOrModifyProductCancelButton;
+		private System.Windows.Forms.Label addOrModifyProductLabel;
+		private System.Windows.Forms.Label idLabel;
+		private System.Windows.Forms.Label nameLabel;
+		private System.Windows.Forms.Label inventoryLabel;
+		private System.Windows.Forms.Label priceLabel;
+		private System.Windows.Forms.Label minLabel;
+		private System.Windows.Forms.Label maxLabel;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
 	}
 }
